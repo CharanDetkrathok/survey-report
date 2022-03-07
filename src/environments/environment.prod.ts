93eda3254bc7fcd25435c10bf1427898bc72c7b2
Charan Detkrathok <charandetkrathok@gmail.com>
2022-03-07 14:39:45 +0700

    changed git ignore...

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  BASE_URL: `http://localhost:8881/`,
+  // BASE_URL: `https://backend-services.ru.ac.th/survey-report-api/`,
+  AUTHENTICATION: `auth-student/authentication`,
+  AUTHORIZATION: `auth-student/authorization`,
+  UN_AUTHORIZATION: `auth-student/un-authorization`,
+  REFRESH_AUTHENTICATION: `auth-student/refresh-authentication`,
+  DISCLOSURE: `student/disclosures`,
+  HEADER: `student/headers`,
+  BECHELOR_ARTICLE_AND_CHOICE: `student/bechelor-articles-choices`,
+  BECHELOR_PART_OF_ARTICLE: `student/bechelor-parts-articles`,
+};
